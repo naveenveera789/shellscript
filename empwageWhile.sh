4af50ef1cd124af0e309c95e4841dffd83d6e7db
naveenveera789 <90519139+naveenveera789@users.noreply.github.com>
2021-10-10 12:18:33 +0530

    empwageWhile.sh

---
+++ empwageWhile.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+
+# CONSTANTS FOR THE PROGRAM
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=10;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+# VARIABLES
+totalEmpHr=0;
+totalWorkingDays=0;
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+        ((totalWorkingDays++))
+        empCheck=$((RANDOM%3));
+        case $empCheck in
+                $IS_FULL_TIME) empHrs=8;;
+                $IS_PART_TIME) empHrs=4;;
+                *) empHrs=0;;
+        esac
+        totalEmpHrs=$(($totalEmpHrs+$empHrs))
+done
+
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
