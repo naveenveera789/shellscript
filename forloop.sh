424970e71519664419ade27750ed43ff2aab7380
naveenveera789 <90519139+naveenveera789@users.noreply.github.com>
2021-10-10 12:28:25 +0530

    forloop.sh

---
+++ forloop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+for file in 'ls *.txt';
+do
+        folderName= 'echo $file | awk -F. '{ print $1 }'';
+        #echo " checking for already existing directory started";
+
+        if [ -d $folderName ];
+        then
+                rm -R $folderName;
+        fi
+        #echo creating folder ${folderName};
+        mkdir $folderName;
+        #echo copying ${file} to ${folderName};
+        cp $file $folderName;
+
+done
