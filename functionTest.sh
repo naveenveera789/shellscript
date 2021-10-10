4c63c2e71dfd278c79d8d9e56a80f61433edaec5
naveenveera789 <90519139+naveenveera789@users.noreply.github.com>
2021-10-10 12:30:43 +0530

    functionTest.sh

---
+++ functionTest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+function myfunc()
+        {
+            echo $1
+         }
+ result="$(myfunc $((RANDOM%2)) )"
+ if [ $result -eq 1 ]
+ then
+         echo "success"
+ else
+         echo "failure"
+ fi
