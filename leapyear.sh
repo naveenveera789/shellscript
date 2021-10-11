05173b795ff8797127bff94dc266d824118c5378
naveenveera789 <90519139+naveenveera789@users.noreply.github.com>
2021-10-11 09:58:19 +0530

    leapyear.sh

---
+++ leapyear.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+echo "Enter the year :"
+read y
+
+a=$(( $y %4 ))
+b=$(( $y %100 ))
+c=$(( $y %400 ))
+
+if [ $a -eq 0 -a $b -ne 0 -o $c -eq 0 ]
+then
+        echo "$y is a leap year"
+else
+        echo "$y is not a leap year"
+fi
